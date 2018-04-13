4b30f89a266f0527772221c380ff4f3fa15b39ee
frank yviray <frankyviray@yahoo.com>
2018-04-13 14:22:48 -0700

    asdf

--- public/js/app.js
+++ public/js/app.js
@@ -14,21 +14,33 @@ $(document).ready(function () {
 
     $.get("/articles/"+id).then(function (data) {
       console.log(data);
-      // The title of the article
+     
       $("#comments").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
+
       $("#comments").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
+
       $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
+    
       $("#comments").append("<button data-id='" + data._id + "' id='savecomment'>Save Comment</button>");
       $("#comments").append("<button data-id='" + data.comments[0]._id + "' id='deletecomment'>Delete Comment</button>");
 
-      // If there's a note in the article
+ 
       if (data.comments) {
-        // Place the title of the note in the title input
+       
         $("#titleinput").val(data.comments[0].title);
-        // Place the body of the note in the body textarea
+ 
+        $("#bodyinput").val(data.comments[0].body);
+      }
+    });
+  });
+
+  $("button.save").on("click", function () {
+    var thisId = $(this).attr("data-id");
+    var thisSave = $(this).attr("data-saved");
+    $.post("/api/saved/"+ thisId,{saved: thisSave})
+      .then(function(data) {
+        console.log(data);
+    });
         $("#bodyinput").val(data.comments[0].body);
       }
     });
--- routes/api-saved-routes.js
+++ routes/api-saved-routes.js
@@ -5,13 +5,13 @@ module.exports = function (app) {
     app.get("/saved", function (req, res) {
         db.Article.find({})
             .then(function (dbArticle) {
-                // If we were able to successfully find Articles, send them back to the client
+        
                 res.render("saved", {
                     array: dbArticle
                 });
             })
             .catch(function (err) {
-                // If an error occurred, send it to the client
+
                 res.json(err);
             });
     })
